5210b30f5d6aa3d94e54d8f35c139cfd4662da66
snehakashyap09 <snehakashyap0928@gmail.com>
2025-06-27 17:23:46 +0530

    axios

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -19,7 +19,7 @@ const navigate = useNavigate()
   e.preventDefault();
   try {
     const { data } = await axios.post(
-      "http://localhost:4000/api/v1/user/login",
+      "https://job-portal-seven-gilt.vercel.app/api/v1/user/login",
       { email, password, role },
       {
         headers: {
--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -10,10 +10,9 @@ function Footer() {
     <footer className= {isAuthorized ? "footerShow" : "footerHide"}>
 <div>&copy; All Rights Reserved by Sneha.</div>
 <div>
-  <Link to={'https://github.com/exclusiveabhi'} target='github'><FaGithub></FaGithub></Link>
-  <Link to={'https://leetcode.com/u/exclusiveabhi/'} target='leetcode'><SiLeetcode></SiLeetcode></Link>
-  <Link to={'https://www.linkedin.com/in/abhishek-rajput-/'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
-  <Link to={'https://www.instagram.com/exclusiveabhi/'} target='instagram'><RiInstagramFill></RiInstagramFill></Link>
+  <Link to={'https://github.com/snehakashyap09'} target='github'><FaGithub></FaGithub></Link>
+  <Link to={'https://leetcode.com/Sneha-Kashyap/'} target='leetcode'><SiLeetcode></SiLeetcode></Link>
+  <Link to={'https://www.linkedin.com/in/sneha-kashyap-5428b62b0-/'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
 </div>
       
     </footer>
