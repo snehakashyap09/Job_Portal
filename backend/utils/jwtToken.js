2cb11c8bdaeafa7300fedaf896f1ea1b8cdcc3a9
snehakashyap09 <snehakashyap0928@gmail.com>
2025-06-27 15:49:27 +0530

    job portal

---
+++ backend/utils/jwtToken.js
@@ -0,0 +1,20 @@
+export const sendToken = (user,statusCode,res,message)=>{
+    console.log("COOKIE_EXPIRE:", process.env.COOKIE_EXPIRE);
+
+    const token  = user.getJWTToken();
+    const options = {
+        expires:new Date(
+            Date.now() + Number(process.env.COOKIE_EXPIRE)*24*60*60*1000 || 17
+        ),
+        httpOnly:true,
+    }
+    console.log("COOKIE_EXPIRE", process.env.COOKIE_EXPIRE)
+    console.log("Date value", new Date(Date.now() + Number(process.env.COOKIE_EXPIRE) * 24 * 60 * 60 * 1000))
+    
+    res.status(statusCode).cookie("token",token,options).json({
+        success:true,
+        user,
+        message,
+        token
+    })
+}
