d179f4326feac8329c5c4a78fb0addc8338f570f
snehakashyap09 <snehakashyap0928@gmail.com>
2025-06-28 09:15:09 +0530

    jwt

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -7,6 +7,8 @@ export const sendToken = (user,statusCode,res,message)=>{
             Date.now() + Number(process.env.COOKIE_EXPIRE)*24*60*60*1000 || 17
         ),
         httpOnly:true,
+        secure: process.env.NODE_ENV === "production",  // ensure HTTPS
+  sameSite: "None", 
     }
     console.log("COOKIE_EXPIRE", process.env.COOKIE_EXPIRE)
     console.log("Date value", new Date(Date.now() + Number(process.env.COOKIE_EXPIRE) * 24 * 60 * 60 * 1000))
